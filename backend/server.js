0b690ff570361ab1ea7e1943c7dc4f89ae6d3b78
Aditya07code <118747765+Aditya07code@users.noreply.github.com>
2024-09-18 11:56:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,10 @@ const mongoose = require('mongoose')
 
 const app = express();
 const PORT = process.env.PORT || 5000
-app.use(cors());
-const allowedOrigins = [process.env.FRONTEND_URL];  
+app.use(cors()); 
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: "https://to-do-app-frontend-9kmu.onrender.com/",
 }));
 app.use(express.json());
 // app.use(express.urlencoded({extended:true}));
