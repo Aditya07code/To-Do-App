915d0e9986da891b7630682aa0ebcf0a11c0ae6d
Aditya07code <aditya1807gupta@gmail.com>
2024-09-18 10:39:42 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const routes = require("./routes/ToDoRoutes");
+require("dotenv").config();
+const cors = require('cors');
+const mongoose = require('mongoose')
+// const path = require('path')
+// const cookieParser = require('cookie-parser');
+// const bcrypt = require('bcrypt')
+// const jwt = require('jsonwebtoken')
+// const upload = require('./config/multerconfig');
+
+
+const app = express();
+const PORT = process.env.PORT || 5000
+app.use(cors());
+// app.set("view engine","ejs");
+app.use(express.json());
+// app.use(express.urlencoded({extended:true}));
+// app.use(express.static(path.join(__dirname,"public")));
+// app.use(cookieParser());
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+app.use("/api", routes);
+
+
+
+
+
+
+app.listen(PORT,()=> console.log(`listening at ${PORT}`));
